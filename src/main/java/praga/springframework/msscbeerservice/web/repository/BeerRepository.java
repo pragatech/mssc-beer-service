6b6fba6a50150330c3447df70d56678f615b30f0
pragatech <pragatheeswaran.a.c@outlook.in>
2023-12-31 22:46:21 +0530

    adding beer JPA repository. closes #6

---
+++ src/main/java/praga/springframework/msscbeerservice/web/repository/BeerRepository.java
@@ -0,0 +1,10 @@
+package praga.springframework.msscbeerservice.web.repository;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import praga.springframework.msscbeerservice.web.domain.Beer;
+
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+
+}
