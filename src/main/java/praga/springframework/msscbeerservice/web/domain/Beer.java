464df0abb1c40a909039be43bcdcc4fd5534ebad
pragatech <pragatheeswaran.a.c@outlook.in>
2024-01-01 14:42:33 +0530

    added beer loader. closes #7

--- src/main/java/praga/springframework/msscbeerservice/web/domain/Beer.java
+++ src/main/java/praga/springframework/msscbeerservice/web/domain/Beer.java
@@ -20,7 +20,7 @@ public class Beer {
     @Id
     @GeneratedValue(generator = "UUID")
     @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
-    @Column(length = 36, columnDefinition = "vatchar", updatable = false, nullable = false)
+    @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
     private UUID id;
     @Version//provide optimistic locking
     private Long version;
--- src/main/java/praga/springframework/msscbeerservice/web/repository/BeerRepository.java
+++ src/main/java/praga/springframework/msscbeerservice/web/repository/BeerRepository.java
@@ -1,10 +1,11 @@
 package praga.springframework.msscbeerservice.web.repository;
 
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import praga.springframework.msscbeerservice.web.domain.Beer;
 
 import java.util.UUID;
 
-public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID>, CrudRepository<Beer, UUID> {
 
 }
